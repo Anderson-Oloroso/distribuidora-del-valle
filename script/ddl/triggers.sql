5f767e58ba512e1691054f212abe5b65f7f1f1e6
Anderson-Oloroso <henrikog4@gmail.com>
2026-08-13 08:56:53 -0600

    :sparkles: feat(trigger): Add trigger to create records on table auditoria_precios when update table productos.precio

--- script/ddl/triggers.sql
+++ script/ddl/triggers.sql
@@ -1,3 +1,20 @@
 --  Al insertar un detalle de pedido, descuenta automáticamente la cantidad vendida del stock.
 
 -- Al actualizar el campo precio en la tabla productos, registra la fecha, el precio anterior y el nuevo en una tabla auditoria_precios.
+DELIMITER //
+CREATE TRIGGER tr_auditar_cambio_precio
+AFTER UPDATE ON productos 
+FOR EACH ROW
+BEGIN 
+	IF NEW.precio > 0 AND OLD.precio <> NEW.precio THEN
+		INSERT INTO auditoria_precios(id_producto, precio_anterior, precio_actual)
+			VALUES (NEW.id_producto, OLD.precio, NEW.precio);
+    END IF;
+END //
+DELIMITER ;
+
+DESC productos;
+SELECT * FROM productos;
+UPDATE productos SET precio = 12.5 WHERE id_producto = 1;
+
+SELECT * FROM auditoria_precios;
