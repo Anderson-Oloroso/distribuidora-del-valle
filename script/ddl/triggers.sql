850ab0ec48e5026ae0594a8cb96dfd32304f1d3d
Anderson-Oloroso <henrikog4@gmail.com>
2026-08-13 07:05:00 -0600

    :memo: Docs(ddl): Add instructions for triggers

---
+++ script/ddl/triggers.sql
@@ -0,0 +1,3 @@
+--  Al insertar un detalle de pedido, descuenta automáticamente la cantidad vendida del stock.
+
+-- Al actualizar el campo precio en la tabla productos, registra la fecha, el precio anterior y el nuevo en una tabla auditoria_precios.
